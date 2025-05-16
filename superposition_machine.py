ea0f95a00258d76c506a7d12e6b5d26631547879
gw67-alt <gw67656@gmail.com>
2025-05-16 21:57:33 +1000

    Update superposition_machine.py

--- superposition_machine.py
+++ superposition_machine.py
@@ -364,7 +364,7 @@ class MainWindow(QMainWindow):
 
                         guess_display_message = f"Analysis: {comparison_text}. --> Preset Output: {current_preset_output}"
                         calc = 1 if is_above else 0
-                        if calc*9 == 9: #superposition example, change '== 9' to either 9 or 1 yet still watch the credits rise(of questionable stability...)
+                        if calc*9 == 9: #superposition example, change '== 9' to either 9 or 0 yet still watch the credits rise(of questionable stability...)
                             game_state["credits"] += WIN_CREDITS  
                         else:
                             game_state["credits"] -= WIN_CREDITS  
